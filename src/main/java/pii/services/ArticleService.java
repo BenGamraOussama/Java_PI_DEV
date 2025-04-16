cc2b3695ccbd56e056c76d4bd8bd757f34beb971
zouaouifarahh <zouaoui.farah@esprit.tn>
2025-04-16 14:34:52 +0100

    Adding IService Interface

--- src/main/java/pii/services/ArticleService.java
+++ src/main/java/pii/services/ArticleService.java
@@ -1,4 +1,33 @@
 package pii.services;
 
-public class ArticleService {
+import pii.entities.Article;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ArticleService implements IService<Article> {
+    @Override
+    public void ajouter(Article p) throws SQLException {
+        
+    }
+
+    @Override
+    public void modifier(Article p) throws SQLException {
+
+    }
+
+    @Override
+    public void supprimer(int id) throws SQLException {
+
+    }
+
+    @Override
+    public Article getOneById(int id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Article> getAll() throws SQLException {
+        return List.of();
+    }
 }
